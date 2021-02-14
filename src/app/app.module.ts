0e0c5470648758a249fe55ae176a32fa1daea6e9
caio <caio.ferraz@souunit.com.br>
2021-02-14 12:27:41 -0300

    Ajustando paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
 import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+
 import {
   AgmCoreModule
 } from '@agm/core';
--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -10,7 +10,6 @@ import { TypographyComponent } from '../../typography/typography.component';
 
 
 import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -37,7 +36,7 @@ import {MatSelectModule} from '@angular/material/select';
     TableListComponent,
     TypographyComponent,
     NotificationsComponent,
-    UpgradeComponent,
+    
   ]
 })
 
